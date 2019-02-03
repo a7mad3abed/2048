c0dda296a976322c888a5dc867e24150fcd1094e
a7mad3abed <a7mad3abed@gmail.com>
2019-02-03 15:19:40 +0100

    debianMerg

--- my2048/my2048/my2048.cpp
+++ my2048/my2048/my2048.cpp
@@ -1,40 +1,63 @@
 #include <iostream>
 #include <vector>
+#include <thread>
 
-constexpr int rowLength = 6;
+constexpr int rowLength = 8;
 
-class Cell {
-public:
-    bool isEmpty = false;
-    int value() const { return mValue; }
-    void setValue(int val) { mValue = val; }
-private:
-    int mValue = 0;
+class Cell{
+	public:
+		int value()const{return mValue;}
+		void setValue(int val){mValue = val;}
+	private:
+		int mValue = 0;
 };
 
-int main(int argc, char **argv)
+std::vector<Cell> row{rowLength};
+
+
+void left()
 {
-
-    std::vector<Cell> row(rowLength);
-    row[0].setValue(4);
-    row[1].setValue(8);
-    row[3].setValue(8);
-
-
-    for (int i = 0; i < rowLength; i++) {
-        if (row[i].value() == 0) {
-            for (int j = i + 1; j < rowLength;) {
-                if (row[j].value() == 0)
-                    continue;
-                if (row[j].value() > 0) {
-                    row[i].setValue(row[j].value());
-                    row[j].setValue(0);
-                    break;
-                }
-            }
-        }
-    }
-    for (int i = 0; i < row.size(); i++)
-        std::cout << row[i].value() << "  ";
-    std::cin.get();
+	for (int i = 0; i < rowLength; i++){
+		if(row[i].value() == 0){
+			for(int j = i+1; j<rowLength;j++){
+				if(row[j].value() >0){
+					row[i].setValue(row[j].value());
+					row[j].setValue(0);
+					break;
+				}
+			}
+		}
+	}
 }
+void display(){
+	while(1){
+		system("clear");
+		for (int i = 0; i < row.size();i++)
+			std::cout << row[i].value() << "  ";
+		std::cout << std::endl;
+	}
+
+}
+void choose(){
+	char c;
+	while(std::cin >> c){
+		if(c == 'r') left();
+		if(c == 'q') exit(0);
+	}
+}
+int main()
+{
+	row[2].setValue(4); 
+	row[5].setValue(2);
+	row[6].setValue(8);
+	row[7].setValue(4);
+
+
+	std::thread view (display);
+	std::thread chos (choose);
+
+	view.join();
+	chos.join();
+}
+
+
